652ecb047686f2d5503c7b1a0f2aefb0c19c1114
javad pournosrat <pournosrat.javad@gmail.com>
2021-05-01 00:40:21 +0430

    input and output are done!

--- run.py
+++ run.py
@@ -3,6 +3,15 @@
 import pyshorteners
 
 url = input(str("enter your url:"))
-s = pyshorteners.Shortener()
-short = s.tinyurl.short(url)
-print(short)
+def surl():
+	s = pyshorteners.Shortener()
+	short = s.tinyurl.short(url)
+	print(short)
+
+def check():
+	if url[0:4] == "http":
+		print("True")
+		surl()
+	else:
+		print("url is wrong!")
+check()
