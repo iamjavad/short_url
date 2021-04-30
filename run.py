fb270d0dc3b5449f01fa0b9d670b31223e55d323
javad pournosrat <pournosrat.javad@gmail.com>
2021-05-01 00:43:08 +0430

    add comments and remove a wrong function!

--- run.py
+++ run.py
@@ -2,16 +2,20 @@
 
 import pyshorteners
 
+#input!
+
 url = input(str("enter your url:"))
+
+#short link function
 def surl():
 	s = pyshorteners.Shortener()
 	short = s.tinyurl.short(url)
 	print(short)
 
-def check():
-	if url[0:4] == "http":
-		print("True")
-		surl()
-	else:
-		print("url is wrong!")
-check()
+#check url
+
+if url[0:4] == "http":
+	print("True")
+	surl()
+else:
+	print("url is wrong!")
